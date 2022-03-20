7e084fe1f986ef861a622e6fb1cec61e3e5ab096
AkaOckha <yakopl@hotmail.com>
2022-03-20 21:02:18 +0300

    add case

---
+++ switch-case.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  String notDegeri = 'BA';
+
+  switch (notDegeri) {
+    case 'AA':
+      print("Notunuz 90 - 100 aralığındadır");
+      break;
+    case 'BA':
+      print("Notunuz 70-80 aralığındadır");
+      break;
+    default:
+      {
+        print("hatalı değer girdiniz.");
+      }
+  }
+}
